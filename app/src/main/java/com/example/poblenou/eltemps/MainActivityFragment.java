6571847af98197007c1f472643222587b113a346
Manuel <41003840s@iespoblenou.org>
2015-10-19 19:14:51 +0200

    Commit inicial (ListView)

--- app/src/main/java/com/example/poblenou/eltemps/MainActivityFragment.java
+++ app/src/main/java/com/example/poblenou/eltemps/MainActivityFragment.java
@@ -5,11 +5,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    ListView listaTiempo;
+    ArrayList items;
+    ArrayAdapter adapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +26,23 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View fragment = inflater.inflate(R.layout.fragment_main, container, false);
+
+        listaTiempo = (ListView) fragment.findViewById(R.id.listView);
+
+        String data [] ={"Lun 26/10 - Soleado", "Mar 27/10 - Niebla", "Mier 26/10 - Nublado", "Jue 26/10 - Lluvioso",
+                "Lun 26/10 - Soleado", "Sab 26/10 - Parcialmente nublado", "Dom 26/10 - Soleado"};
+
+        items = new ArrayList(Arrays.asList(data));
+        adapter = new ArrayAdapter<String>(getContext(),
+                R.layout.filas_dias, R.id.textView, items);
+        listaTiempo.setAdapter(adapter);
+
+
+
+
+        return fragment;
+
     }
 }
